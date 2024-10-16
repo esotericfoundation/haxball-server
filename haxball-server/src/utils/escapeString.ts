aa35a29313d1190a910f912432c34fa92a6a45a1
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-16 22:29:35 +0100

    Delete folder

--- haxball-server/src/utils/escapeString.ts
+++
@@ -1,5 +0,0 @@
-export const escapeString = (str: unknown): typeof str => {
-	if (typeof str !== "string") return str;
-
-	return str.replace(/"/g, '\\"');
-};
--- haxball-server/src/utils/types.ts
+++
@@ -1,7 +0,0 @@
-import { ChatInputCommandInteraction, Client, SlashCommandBuilder, SlashCommandOptionsOnlyBuilder } from "discord.js";
-import { ControlPanel } from "../ControlPanel";
-
-export type Command = {
-    data: SlashCommandBuilder|Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">|SlashCommandOptionsOnlyBuilder,
-    execute (interaction: ChatInputCommandInteraction, client: Client, panel: ControlPanel): Promise<unknown>
-}
